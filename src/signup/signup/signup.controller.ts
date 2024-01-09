f848ee3640db26ed9fa28284d653861ade583660
aswin <aswinedassery@gmail.com>
2024-01-09 07:41:36 +0530

    added ejs template

--- src/signup/signup/signup.controller.ts
+++ src/signup/signup/signup.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { SignupsrvService } from './signupsrv.service';
 import { UserInfo } from 'src/dto/schema/signup';
 
 @Controller('signup')
 export class SignupController {
   constructor(private signupService: SignupsrvService) {}
+
+  @Get()
+  @Render('register')
+  loadSignup() {}
+
   @Post()
   async postSignup(@Body() body: UserInfo) {
     const newUser = await this.signupService.createUser(body);
